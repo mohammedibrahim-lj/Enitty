dfbaaafd845ff1f2dd803bb1c6ef06ae79727f07
mohammedibrahim-lj <mohammedibrahim.lj@gmail.com>
2015-12-28 12:43:15 -0800

    entity Demo

---
+++ Entity/Controllers/StudentTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entity.Models;
+
+namespace Entity.Controllers
+{
+    public class StudentTablesController : Controller
+    {
+        private StudentDB db = new StudentDB();
+
+        // GET: StudentTables
+        public ActionResult Index()
+        {
+            return View(db.StudTable.ToList());
+        }
+
+        // GET: StudentTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudTable.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentTabs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,StudentName,StudentAge")] StudentTable studentTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudTable.Add(studentTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudTable.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentID,StudentName,Age")] StudentTable studentTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudTable.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentTable studentTable = db.StudTable.Find(id);
+            db.StudTable.Remove(studentTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Entity/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Entity.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Entity.Models.StudentDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Entity.Models.StudentDB";
+        }
+
+        protected override void Seed(Entity.Models.StudentDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
